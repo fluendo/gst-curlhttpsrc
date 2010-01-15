4f94b78038487654d3d2fce9e33552811a60b016
Jorge Zapata <jzapata@fluendo.com>
2010-01-15 16:26:09 +0000

    + Add the demo version description in case the demo compile flag is set
    git-svn-id: https://core.fluendo.com/gstreamer/svn/trunk/common@1778 2b0047a9-a6d8-0310-accf-f7200b2a168c

--- gst-fluendo.h
+++ gst-fluendo.h
@@ -2,7 +2,14 @@
 #define _INCLUDE_NOPIC_H_
 
 #ifdef ENABLE_STATIC_PLUGIN
-
+#if ENABLE_DEMO_PLUGIN
+#define FLUENDO_PLUGIN_DEFINE(major, minor, name, fun, desc, init, version, \
+  license, pkg, url) \
+    gboolean gst_##fun##_init_static() { \
+      return gst_plugin_register_static(major, minor, name, desc " [Demo Version]", init, \
+          version, license, "Fluendo",pkg,url); \
+    }
+#else
 #define FLUENDO_PLUGIN_DEFINE(major, minor, name, fun, desc, init, version, \
   license, pkg, url) \
     gboolean gst_##fun##_init_static() { \
@@ -10,13 +17,18 @@
           version, license, "Fluendo",pkg,url); \
     }
 
+#endif /* ENABLE_DEMO_PLUGIN */
 #else
-
+#if ENABLE_DEMO_PLUGIN
+#define FLUENDO_PLUGIN_DEFINE(major, minor, name, fun, desc, init, version, \
+  license,pkg,url) \
+  GST_PLUGIN_DEFINE(major, minor, name, desc " [Demo Version]", init, version, license, pkg, url)
+#else
 #define FLUENDO_PLUGIN_DEFINE(major, minor, name, fun, desc, init, version, \
   license,pkg,url) \
   GST_PLUGIN_DEFINE(major, minor, name, desc, init, version, license, pkg, url)
-
-#endif
+#endif /* ENABLE_DEMO_PLUGIN */
+#endif /* ENABLE_STATIC_PLUGIN */
 
 #if ENABLE_DEMO_PLUGIN
 
