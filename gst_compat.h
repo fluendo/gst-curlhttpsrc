8757f77c8b1dde2f5682da4f843fe295c73d6c14
Julien Moutte <julien@fluendo.com>
2012-05-15 16:59:00 +0000

    2012-05-15  Julien Moutte <julien@fluendo.com>
    	* gst-compat.h: Add some more macros.
    	* Makefile.am: Distribute the compat header.
    
    git-svn-id: https://core.fluendo.com/gstreamer/svn/trunk/common@2286 2b0047a9-a6d8-0310-accf-f7200b2a168c

--- gst_compat.h
+++ gst_compat.h
@@ -5,6 +5,7 @@
 
 #ifndef GST_COMPAT_H
 #define GST_COMPAT_H
+
 #include <gst/gst.h>
 
 #ifndef GST_CHECK_VERSION
@@ -22,6 +23,19 @@
           GST_ELEMENT_DETAILS (longname,classification,description,author);   \
       gst_element_class_set_details (element_class, &details);                \
     }G_STMT_END
+#endif
+
+#if !GST_CHECK_VERSION(0,10,23)
+#define GST_BUFFER_FLAG_MEDIA1 (GST_MINI_OBJECT_FLAG_LAST << 5)
+#define GST_BUFFER_FLAG_MEDIA2 (GST_MINI_OBJECT_FLAG_LAST << 6)
+#define GST_BUFFER_FLAG_MEDIA3 (GST_MINI_OBJECT_FLAG_LAST << 7)
+
+#define GST_MEMDUMP(_title, _data, _length) while (0)
+#define GST_MEMDUMP_OBJECT(_object, _title, _data, _length) while (0)
+#endif
+
+#if !GST_CHECK_VERSION(0,10,33)
+#define GST_BUFFER_FLAG_MEDIA4 GST_MINI_OBJECT_FLAG_RESERVED1
 #endif
 
 #endif /* GST_COMPAT_H */
